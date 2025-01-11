254cc9e7300ba01eddadbae00ec970a97584eab6
Satyam-kr07 <kumarsatyam7605@gmail.com>
2025-01-11 18:57:15 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+// basic input & output
+// int main (){
+//     int x;
+//     cin>>x;
+//     cout<<"value of x : " << x << endl;
+//     return 0;
+// }
+
+// if else
+// q. to find whether you're an adult or not,
+// int main (){
+//     int age;
+//     cin>>age;
+//     if (age>=18)
+//     {
+//         cout<<"you are adult"<<endl;
+//     }
+//     else{
+//         cout<< "you're not an adult"<<endl;
+//     }
+
+//     return 0;
+// }
+
+// for loops
+
+// int main (){
+//     for (int i = 0; i < 5; i++)
+//     {
+//         cout<<"Hey Satyam,Stay Strong"<<endl;
+//     }
+//  return 0;   
+// }
+
+// functions
+// 1. void functions
+ 
+//  void printname (){
+//     cout<<"Hey Satyam!" <<endl;
+//  }
+
+// int main (){
+//      printname();
+//      return 0;
+// }
+
+// 2. Parametrized function
+//   void printname ( string name){
+//     cout<< name << endl;
+//   }
+// int main (){
+//     string name;
+//     cin>>name;
+//     printname(name);
+    
+//     string name2;
+//     cin>>name2;
+//     printname(name2);
+//     return 0;
+// }
+
+// 3. Return function
+
+//   int sum(int num1,int num2) {
+//     int num3= num1+num2;
+//     return num3;
+//   }
+
+//  int main (){
+//     int num1,num2;
+//     cin>>num1>>num2;
+//   int res= sum(num1,num2);
+//   cout<< res;
+//   return 0;
+//  }
+
+// C++ STL
+ // pair done (It lies inside utility library)
+
+ // 1. Cointainer
+ // Vectors
