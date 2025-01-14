1f4918de51871c33cd15a270974feb3f620fc0db
Satyam-kr07 <kumarsatyam7605@gmail.com>
2025-01-14 20:51:23 +0530

    Add files via upload

---
+++ Time complexity.cpp
@@ -0,0 +1,56 @@
+// Time complexity != Time taken
+// rate at which time taken increases wrt to input
+// Time complexity is finded in worst case senario
+// AVoid constants
+// avoid lower values
+for (int i = 1; i < n; i++)
+{
+    cout<<"hey"<<endl;
+}
+// In this loop 3 things are happening, increment(i++),check (i<5) & print.
+// So, hey is printed 5 times & on each step 3 things are happening. 5*3=15. so 15 things in total
+// Time complexity = O(15) or preffered way , O(n*3)= O(n)
+
+
+//O(4n*3+3n*2+8) = O(n*3+n*2)= O(n*3)
+
+// questions :- 
+// In nested loop, no. of loops encoded inside starts getting multiplied to the number of times they're running.
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; i < n i++)
+    {
+        /* code */
+    }
+    
+}
+// Complexity = O(n*n) = O(n*2)
+
+// ----------------------------------------
+
+// Space complexity
+
+// Auxiliary space(space to solve the problem) + input space (space to store the problem)
+
+// if c=a+b, then c is a auxiliary space
+// array of size n, O(N) is a space complexity
+// never do anything to the input
+
+// In an interview if time complexxity is given as 1s then consider is as 10*8 (raise to the power)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
