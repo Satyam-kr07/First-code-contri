d693aaa7ad8309f0acca71f5550c554b933d6a46
Satyam-kr07 <kumarsatyam7605@gmail.com>
2025-01-28 11:34:33 +0530

    Update Time complexity.cpp

--- Time complexity.cpp
+++ Time complexity.cpp
@@ -6,7 +6,7 @@
 for (int i = 1; i < n; i++)
 {
     cout<<"hey"<<endl; hu
-}
+} kj
 // In this loop 3 things are happening, increment(i++),check (i<5) & print.
 // So, hey is printed 5 times & on each step 3 things are happening. 5*3=15. so 15 things in total
 // Time complexity = O(15) or preffered way , O(n*3)= O(n)
